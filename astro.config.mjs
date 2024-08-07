d16ccd836261d1562ad97e7970225f8520c774d2
Daniel Monettelli <danielmonetelli@hotmail.com>
2024-08-07 13:55:40 -0500

    🚀 [Code] Added CSS styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,33 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://mauiuxguide.github.io',
-	base: '',
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/mauiuxguide/mauiuxguide.github.io',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  site: "https://mauiuxguide.github.io",
+  base: "",
+  integrations: [
+    starlight({
+      title: "MauiUX Guide",
+      social: {
+        github: "https://github.com/mauiuxguide/mauiuxguide.github.io",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+      customCss: [
+        // Relative path to your custom CSS file
+        "./src/styles/custom.css",
+      ],
+    }),
+  ],
 });
