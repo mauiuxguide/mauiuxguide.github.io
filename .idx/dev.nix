52ecb282b7200e53417b82716570ea35faf38054
Daniel Monettelli <danielmonetelli@hotmail.com>
2024-08-17 02:21:44 -0500

    ⬆️ [Code] Updated dependencies s1

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -4,7 +4,7 @@
   # Which nixpkgs channel to use.
   channel = "stable-23.11"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
-  packages = [ pkgs.nodejs_20  pkgs.pnpm-lock-export];
+  packages = [ pkgs.nodejs_20  pkgs.pnpm-lock-export pkgs.pnpm-lock-export];
   # Sets environment variables in the workspace
   env = { };
   idx = {
